ebf830dc598b834b8c8cd4c56a53bb9f8c060366
lucadibaa <lucadibaa@protonmail.com>
2022-02-25 10:22:10 +0100

    chore: router progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,19 @@ import NotificationProvider from '../components/assets/notifications/Notificatio
 import store from '../redux/store'
 import { useEffect } from 'react'
 import { isTokenValid } from '../redux/userActions'
+import ProgressBar from "@badrap/bar-of-progress"
+import Router from 'next/router'
+
+const progress = new ProgressBar({
+  size: 3,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 80,
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 function MyApp({ Component, pageProps }) {
 
