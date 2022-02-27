93d91d4d42697bb63fcaef052422e5b0442ae5ba
lucadibaa <lucadibaa@protonmail.com>
2022-02-27 15:31:56 +0100

    chore: reservation card

--- pages/reservations.js
+++ pages/reservations.js
@@ -4,13 +4,18 @@ import { useDispatch, useSelector } from "react-redux"
 import { logout } from "../redux/userActions"
 import useWindowSize from "../utils/useWindowSize"
 import { useEffect, useState } from "react"
-import { ChevronDownIcon, ChevronUpIcon, SearchIcon } from "@heroicons/react/outline"
+import { ChevronDownIcon, ChevronUpIcon, CreditCardIcon, SearchIcon, UserGroupIcon, UsersIcon } from "@heroicons/react/outline"
+import { GiAirplaneArrival, GiAirplaneDeparture } from "react-icons/gi"
+import { BiEuro } from "react-icons/bi"
 import Register from "../components/reservations/Register"
 import Login from "../components/reservations/Login"
 import { useRouter } from "next/router"
 import api from "../api/axios"
 import requests from "../api/requests"
 import { useNotification } from '../components/assets/notifications/NotificationProvider'
+import moment from "moment"
+import Image from "next/image"
+import { capitalize } from "../utils/functions"
 
 export const getServerSideProps = async ({ query: { reservationId: id } }) => {
     try {
@@ -40,6 +45,7 @@ const Reservations = ({ reservation, error }) => {
 
     const { user } = useSelector(state => state.user)
     const router = useRouter()
+    const { reservationId } = router.query
 
     const { width } = useWindowSize()
 
@@ -50,7 +56,7 @@ const Reservations = ({ reservation, error }) => {
 
     const handleSubmit = e => {
         e.preventDefault()
-        if (!search.match(/^[0-9a-fA-F]{24}$/)) return
+        if (!search.match(/^[0-9a-fA-F]{24}$/)) return dispatchNotification({ type: "WARNING", message: 'Invalid reservation id' })
         router.replace({
             pathname: '/reservations',
             query: {
@@ -80,6 +86,8 @@ const Reservations = ({ reservation, error }) => {
         data.total += r.total
     })
 
+    console.log(reservation)
+
     return (
         <Layout noBanner>
             <Head>
@@ -88,16 +96,99 @@ const Reservations = ({ reservation, error }) => {
 
             <main className="mt-24 max-w-[90%] mx-auto min-h-[60vh] flex justify-between pb-10 sm:mt-0 sm:pt-4 sm:flex-col-reverse">
 
-                <section className="w-1/3 sm:w-full">
+                <section>
                     {
                         Object.keys(user).length > 0 ?
                             <div>
                                 <h2 className="text-2xl text-left mt-14 mb-6 font-Sofia tracking-wider font-light text-asphalt uppercase lg:mt-10 lg:mb-4 md:my-3">Your Reservations</h2>
-                                <div>
+                                <div className="flex flex-col gap-4">
                                     {
                                         reservations?.map(r => (
-                                            <div key={r._id}>
-                                                {r._id}
+                                            <div key={r._id} className="bg-white border rounded shadow p-3 text-asphalt ">
+                                                <div className="flex items-center justify-between text-sm">
+                                                    <span className="tracking-wide">Reservation N°</span>
+                                                    <span className="text-[13px]">{r._id}</span>
+                                                </div>
+
+                                                <hr className="bg-jungle/10 h-0.5 m-2.5 sm:m-2" />
+
+                                                <div className="flex">
+                                                    <div className="relative w-44 h-28 overflow-hidden">
+                                                        <Image
+                                                            src={'https://res.cloudinary.com/drpbnvds9/image/upload/v1643291972/hotel%20web%20app/rooms%20list/banner_gpixcx.jpg'}
+                                                            alt={r.roomSlug}
+                                                            layout="fill"
+                                                            objectFit="cover"
+                                                            className="rounded"
+                                                        />
+                                                    </div>
+                                                    <div className="flex flex-col gap-1.5 grow px-4">
+
+                                                        <h3 className="font-PlayfairDisplay font-semibold tracking-wide text-camel">{capitalize(r.roomSlug.replaceAll("-", " "))} </h3>
+
+                                                        <div className="flex items-center justify-between text-sm">
+                                                            <div className="flex items-center">
+                                                                <UserGroupIcon className="h-4 mr-2" />
+                                                                <span>N° of Guests</span>
+                                                            </div>
+                                                            <span>{r.guestsNumber}</span>
+                                                        </div>
+
+                                                        <div className="flex items-center justify-between text-sm">
+                                                            <div className="flex items-center">
+                                                                <UserGroupIcon className="h-4 mr-2" />
+                                                                <span>N° of Nights</span>
+                                                            </div>
+                                                            <span>{r.nights}</span>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+
+                                                <hr className="bg-jungle/10 h-0.5 m-2.5 sm:m-2" />
+
+                                                <div className="flex flex-col gap-1.5">
+
+                                                    <div className="flex items-center justify-between text-sm">
+                                                        <div className="flex items-center">
+                                                            <GiAirplaneArrival className="text-base mr-3" />
+                                                            <span>Arrival</span>
+                                                        </div>
+                                                        <span>{moment(r.arrival).format('ddd DD, MMM YYYY')}</span>
+                                                    </div>
+
+                                                    <div className="flex items-center justify-between text-sm">
+                                                        <div className="flex items-center">
+                                                            <GiAirplaneDeparture className="text-base mr-3" />
+                                                            <span>Departure</span>
+                                                        </div>
+                                                        <span>{moment(r.departure).format('ddd DD, MMM YYYY')}</span>
+                                                    </div>
+
+                                                </div>
+
+                                                <hr className="bg-jungle/10 h-0.5 m-2.5 sm:m-2" />
+
+                                                <div className="flex flex-col gap-1.5">
+
+                                                    <div className="flex items-center justify-between text-sm">
+                                                        <div className="flex items-center">
+                                                            <CreditCardIcon className="h-4 mr-3" />
+                                                            <span>Payment</span>
+                                                        </div>
+                                                        <span>{r.payment}</span>
+                                                    </div>
+
+                                                    <div className="flex items-center justify-between text-sm">
+                                                        <div className="flex items-center">
+                                                            <BiEuro className="text-base mr-3" />
+                                                            <span>Total</span>
+                                                        </div>
+                                                        <span>{r.total} €</span>
+                                                    </div>
+
+                                                </div>
+
                                             </div>
                                         ))
                                     }
@@ -112,13 +203,97 @@ const Reservations = ({ reservation, error }) => {
                                         <SearchIcon className="h-5" />
                                     </button>
                                 </form>
-                                <div>
+                                <div className="mt-5">
                                     {
                                         reservation ?
-                                            <div>
-                                                {reservation?._id}
+                                            <div className="bg-white border rounded shadow p-3 text-asphalt ">
+                                                <div className="flex items-center justify-between text-sm">
+                                                    <span className="tracking-wide">Reservation N°</span>
+                                                    <span className="text-[13px]">{reservation._id}</span>
+                                                </div>
+
+                                                <hr className="bg-jungle/10 h-0.5 m-2.5 sm:m-2" />
+
+                                                <div className="flex">
+                                                    <div className="relative w-44 h-28 overflow-hidden">
+                                                        <Image
+                                                            src={'https://res.cloudinary.com/drpbnvds9/image/upload/v1643291972/hotel%20web%20app/rooms%20list/banner_gpixcx.jpg'}
+                                                            alt={reservation.roomSlug}
+                                                            layout="fill"
+                                                            objectFit="cover"
+                                                            className="rounded"
+                                                        />
+                                                    </div>
+                                                    <div className="flex flex-col gap-1.5 grow px-4">
+
+                                                        <h3 className="font-PlayfairDisplay font-semibold tracking-wide text-camel">{capitalize(reservation.roomSlug.replaceAll("-", " "))} </h3>
+
+                                                        <div className="flex items-center justify-between text-sm">
+                                                            <div className="flex items-center">
+                                                                <UserGroupIcon className="h-4 mr-2" />
+                                                                <span>N° of Guests</span>
+                                                            </div>
+                                                            <span>{reservation.guestsNumber}</span>
+                                                        </div>
+
+                                                        <div className="flex items-center justify-between text-sm">
+                                                            <div className="flex items-center">
+                                                                <UserGroupIcon className="h-4 mr-2" />
+                                                                <span>N° of Nights</span>
+                                                            </div>
+                                                            <span>{reservation.nights}</span>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+
+                                                <hr className="bg-jungle/10 h-0.5 m-2.5 sm:m-2" />
+
+                                                <div className="flex flex-col gap-1.5">
+
+                                                    <div className="flex items-center justify-between text-sm">
+                                                        <div className="flex items-center">
+                                                            <GiAirplaneArrival className="text-base mr-3" />
+                                                            <span>Arrival</span>
+                                                        </div>
+                                                        <span>{moment(reservation.arrival).format('ddd DD, MMM YYYY')}</span>
+                                                    </div>
+
+                                                    <div className="flex items-center justify-between text-sm">
+                                                        <div className="flex items-center">
+                                                            <GiAirplaneDeparture className="text-base mr-3" />
+                                                            <span>Departure</span>
+                                                        </div>
+                                                        <span>{moment(reservation.departure).format('ddd DD, MMM YYYY')}</span>
+                                                    </div>
+
+                                                </div>
+
+                                                <hr className="bg-jungle/10 h-0.5 m-2.5 sm:m-2" />
+
+                                                <div className="flex flex-col gap-1.5">
+
+                                                    <div className="flex items-center justify-between text-sm">
+                                                        <div className="flex items-center">
+                                                            <CreditCardIcon className="h-4 mr-3" />
+                                                            <span>Payment</span>
+                                                        </div>
+                                                        <span>{reservation.payment}</span>
+                                                    </div>
+
+                                                    <div className="flex items-center justify-between text-sm">
+                                                        <div className="flex items-center">
+                                                            <BiEuro className="text-base mr-3" />
+                                                            <span>Total</span>
+                                                        </div>
+                                                        <span>{reservation.total} €</span>
+                                                    </div>
+
+                                                </div>
+
                                             </div>
                                             :
+                                            reservationId &&
                                             <div>
                                                 no reservation match that id
                                             </div>
@@ -128,7 +303,7 @@ const Reservations = ({ reservation, error }) => {
                     }
                 </section>
 
-                <section className="">
+                <section>
                     {
                         Object.keys(user).length > 0 ?
                             <>
--- utils/functions.js
+++ utils/functions.js
@@ -1,3 +1,7 @@
+export const capitalize = string => {
+    return string?.toLowerCase().split(' ').map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(' ')
+}
+
 export const toEuropean = date => {
     return date?.replace(/-/g, ' / ').split(' ').reverse().join('')
 }
